29125eb0c24a22de91a7afb42502c8c2c9dffb0f
Matheus Ferreira <111222292+mattfeg@users.noreply.github.com>
2023-10-10 16:14:10 -0300

    Commit Inicial

---
+++ BuscarCoordenadas.py
@@ -0,0 +1,5 @@
+# BUSCAR COORDENADAS
+
+# ENTRADA: Arquivo CSV com os nomes dos Locais.
+
+# SAIDA: Arquivo CSV com a Latitude e Longitude dos Locais.
---
+++ CriarRede.py
@@ -0,0 +1,7 @@
+# CRIAR REDE DE MOVIMENTAÇÃO DAS AIH
+
+# ENTRADA: CSV com os Municípios de Residência e Hospitais de Internação
+
+# SAÍDA: Um arquivo CSV com os nós da Rede;
+# Um arquivo CSV com as arestas da Rede;
+# e a Rede montada no Networkx.
